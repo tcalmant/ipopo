a33ff71a7b96afaac1c227e670842874fd8515fb
Thomas Calmant <thomas.calmant@gmail.com>
2013-10-01 16:58:55 +0200

    Updated setup.py for iPOPO 0.5.4
    - Corrected download link
    - Development status changes from alpha to beta
    - Added Python 3.x classifiers

--- setup.py
+++ setup.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python
 # -- Content-Encoding: UTF-8 --
+"""
+iPOPO installation script
+
+:author: Thomas Calmant
+:copyright: Copyright 2013, isandlaTech
+:license: GPLv3
+:version: 0.5.4
+:status: Alpha
+
+..
+
+    This file is part of iPOPO.
+
+    iPOPO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    iPOPO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with iPOPO. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# Module version
+__version_info__ = (0, 5, 4)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
 
 # ------------------------------------------------------------------------------
+
+import os
 
 try:
     from setuptools import setup
@@ -9,8 +44,6 @@ except ImportError:
     from distutils.core import setup
 
 # ------------------------------------------------------------------------------
-
-import os
 
 def read(fname):
     """
@@ -22,29 +55,36 @@ def read(fname):
 # ------------------------------------------------------------------------------
 
 setup(name='iPOPO',
-      version='0.5.3',
+      version=__version__,
       license='GPLv3',
       description='A service-oriented component model framework',
       long_description=read('README.rst'),
       author='Thomas Calmant',
       author_email='thomas.calmant@gmail.com',
       url='http://ipopo.coderxpress.net/',
-      download_url='http://ipopo.coderxpress.net/ipopo-0.5.zip',
+      download_url='http://ipopo.coderxpress.net/ipopo-{0}.zip'\
+                   .format(__version__),
       packages=['pelix',
-                'pelix.ipopo',
                 'pelix.http',
+                'pelix.internals',
+                'pelix.ipopo',
+                'pelix.ipopo.handlers',
                 'pelix.remote',
                 'pelix.remote.discovery',
                 'pelix.services',
                 'pelix.shell'],
       classifiers=[
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 4 - Beta',
             'Environment :: Console',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
             'Topic :: Software Development :: Libraries :: Application Frameworks'
-      ]
-)
+      ])
