e7525f08662d0b12bb516958568b16ba7d6a6456
Thomas Calmant <thomas.calmant@gmail.com>
2013-10-25 09:35:24 +0200

    Python 2.6 compatibility

--- pelix/ipopo/core.py
+++ pelix/ipopo/core.py
@@ -582,8 +582,7 @@ class _IPopoService(object):
             # Look for the required handlers
             try:
                 handler_factories = {self._handlers[handler_id]
-                                     for handler_id
-                                     in factory_context.get_handlers_ids()}
+                         for handler_id in factory_context.get_handlers_ids()}
 
             except KeyError:
                 raise TypeError("Missing handler '{0}' for factory '{1}', "
