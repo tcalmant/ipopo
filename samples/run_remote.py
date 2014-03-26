010f6cb98a843b42c3abad380354c124dbdd4014
Thomas Calmant <thomas.calmant@gmail.com>
2014-03-26 18:22:51 +0100

    The Remote Services sample uses the iPOPO waiting list
    This allows to easily update implementations and play with components
    without manually instantiating components nor using IPOPO_AUTO_RESTART

--- samples/run_remote.py
+++ samples/run_remote.py
@@ -49,7 +49,7 @@ __docformat__ = "restructuredtext en"
 # ------------------------------------------------------------------------------
 
 # Pelix
-from pelix.ipopo.constants import use_ipopo, IPOPO_AUTO_RESTART
+from pelix.ipopo.constants import use_waiting_list
 import pelix.framework
 import pelix.remote as rs
 
@@ -77,11 +77,10 @@ class InstallUtils(object):
         # Install the bundle
         self.context.install_bundle('pelix.remote.discovery.multicast').start()
 
-        with use_ipopo(self.context) as ipopo:
+        with use_waiting_list(self.context) as ipopo:
             # Instantiate the discovery
-            ipopo.instantiate(rs.FACTORY_DISCOVERY_MULTICAST,
-                              "pelix-discovery-multicast",
-                              {IPOPO_AUTO_RESTART: True})
+            ipopo.add(rs.FACTORY_DISCOVERY_MULTICAST,
+                      "pelix-discovery-multicast")
 
 
     def discovery_mdns(self):
@@ -94,11 +93,10 @@ class InstallUtils(object):
         # Install the bundle
         self.context.install_bundle('pelix.remote.discovery.mdns').start()
 
-        with use_ipopo(self.context) as ipopo:
+        with use_waiting_list(self.context) as ipopo:
             # Instantiate the discovery
-            ipopo.instantiate(rs.FACTORY_DISCOVERY_ZEROCONF,
-                              "pelix-discovery-zeroconf",
-                              {IPOPO_AUTO_RESTART: True})
+            ipopo.add(rs.FACTORY_DISCOVERY_ZEROCONF,
+                      "pelix-discovery-zeroconf")
 
 
     def transport_jsonrpc(self):
@@ -108,14 +106,12 @@ class InstallUtils(object):
         # Install the bundle
         self.context.install_bundle('pelix.remote.json_rpc').start()
 
-        with use_ipopo(self.context) as ipopo:
+        with use_waiting_list(self.context) as ipopo:
             # Instantiate the discovery
-            ipopo.instantiate(rs.FACTORY_TRANSPORT_JSONRPC_EXPORTER,
-                              "pelix-jsonrpc-exporter",
-                              {IPOPO_AUTO_RESTART: True})
-            ipopo.instantiate(rs.FACTORY_TRANSPORT_JSONRPC_IMPORTER,
-                              "pelix-jsonrpc-importer",
-                              {IPOPO_AUTO_RESTART: True})
+            ipopo.add(rs.FACTORY_TRANSPORT_JSONRPC_EXPORTER,
+                      "pelix-jsonrpc-exporter")
+            ipopo.add(rs.FACTORY_TRANSPORT_JSONRPC_IMPORTER,
+                      "pelix-jsonrpc-importer")
 
 
     def transport_jabsorbrpc(self):
@@ -126,14 +122,12 @@ class InstallUtils(object):
         self.context.install_bundle('pelix.remote.transport.jabsorb_rpc') \
                                                                         .start()
 
-        with use_ipopo(self.context) as ipopo:
+        with use_waiting_list(self.context) as ipopo:
             # Instantiate the discovery
-            ipopo.instantiate(rs.FACTORY_TRANSPORT_JABSORBRPC_EXPORTER,
-                              "pelix-jabsorbrpc-exporter",
-                              {IPOPO_AUTO_RESTART: True})
-            ipopo.instantiate(rs.FACTORY_TRANSPORT_JABSORBRPC_IMPORTER,
-                              "pelix-jabsorbrpc-importer",
-                              {IPOPO_AUTO_RESTART: True})
+            ipopo.add(rs.FACTORY_TRANSPORT_JABSORBRPC_EXPORTER,
+                      "pelix-jabsorbrpc-exporter")
+            ipopo.add(rs.FACTORY_TRANSPORT_JABSORBRPC_IMPORTER,
+                      "pelix-jabsorbrpc-importer")
 
 
     def transport_xmlrpc(self):
@@ -143,14 +137,12 @@ class InstallUtils(object):
         # Install the bundle
         self.context.install_bundle('pelix.remote.xml_rpc').start()
 
-        with use_ipopo(self.context) as ipopo:
+        with use_waiting_list(self.context) as ipopo:
             # Instantiate the discovery
-            ipopo.instantiate(rs.FACTORY_TRANSPORT_XMLRPC_EXPORTER,
-                              "pelix-xmlrpc-exporter",
-                              {IPOPO_AUTO_RESTART: True})
-            ipopo.instantiate(rs.FACTORY_TRANSPORT_XMLRPC_IMPORTER,
-                              "pelix-xmlrpc-importer",
-                              {IPOPO_AUTO_RESTART: True})
+            ipopo.add(rs.FACTORY_TRANSPORT_XMLRPC_EXPORTER,
+                      "pelix-xmlrpc-exporter")
+            ipopo.add(rs.FACTORY_TRANSPORT_XMLRPC_IMPORTER,
+                      "pelix-xmlrpc-importer")
 
 # ------------------------------------------------------------------------------
 
@@ -166,6 +158,7 @@ def main(is_server, discoveries, transports, http_port=0):
     # Create the framework
     framework = pelix.framework.create_framework((# iPOPO
                                                   'pelix.ipopo.core',
+                                                  'pelix.ipopo.waiting',
 
                                                   # Shell
                                                   'pelix.shell.core',
@@ -185,17 +178,15 @@ def main(is_server, discoveries, transports, http_port=0):
 
     # Instantiate components
     # Get the iPOPO service
-    with use_ipopo(context) as ipopo:
+    with use_waiting_list(context) as ipopo:
         # Instantiate remote service components
         # ... HTTP, using a random port
-        ipopo.instantiate("pelix.http.service.basic.factory", "http-server",
-                        {"pelix.http.port": http_port,
-                         IPOPO_AUTO_RESTART: True})
+        ipopo.add("pelix.http.service.basic.factory", "http-server",
+                  {"pelix.http.port": http_port})
 
         # ... servlet giving access to the registry
-        ipopo.instantiate(rs.FACTORY_REGISTRY_SERVLET,
-                          "pelix-remote-dispatcher-servlet",
-                          {IPOPO_AUTO_RESTART: True})
+        ipopo.add(rs.FACTORY_REGISTRY_SERVLET,
+                  "pelix-remote-dispatcher-servlet")
 
     # Prepare the utility object
     util = InstallUtils(context)
