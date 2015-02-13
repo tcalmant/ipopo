c1076294e51cfa3ac2024312a6ffa2bf3cb5df83
Thomas Calmant <thomas.calmant@gmail.com>
2015-02-13 10:52:46 +0100

    Added a __main__ module in pelix.shell
    Allows to start a Pelix shell calling:
    $ python -m pelix.shell

---
+++ pelix/shell/__main__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -- Content-Encoding: UTF-8 --
+"""
+Alias to call pelix.shell.console.main()
+
+:author: Thomas Calmant
+:copyright: Copyright 2015, isandlaTech
+:license: Apache License 2.0
+:version: 0.0.1
+:status: Alpha
+
+..
+
+    Copyright 2015 isandlaTech
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+# Module version
+__version_info__ = (0, 0, 1)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+# ------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    from pelix.shell.console import main
+    main()
