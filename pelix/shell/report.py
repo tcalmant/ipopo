dc0be677cbcced117eee4e55e6587e2a2828c113
Thomas <thomas.calmant@gmail.com>
2015-09-02 19:44:56 +0200

    Corrected version number of pelix.shell.report

--- pelix/shell/report.py
+++ pelix/shell/report.py
@@ -4,8 +4,7 @@
 :author: Thomas Calmant
 :copyright: Copyright 2015, isandlaTech
 :license: Apache License 2.0
-:version: 0.6.1
-:status: Alpha
+:version: 0.6.3
 
 ..
 
@@ -25,7 +24,7 @@
 """
 
 # Module version
-__version_info__ = (0, 6, 1)
+__version_info__ = (0, 6, 3)
 __version__ = ".".join(str(x) for x in __version_info__)
 
 # Documentation strings format
