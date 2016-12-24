cbbe7840e1b190194041f0c81b21ff182180fe90
Thomas Calmant <thomas.calmant@gmail.com>
2016-12-24 13:15:56 +0100

    Autodoc configuration: sort members by kind
    Doc is more readable as properties and methods are in different groups

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,9 @@ extensions = [
     'sphinx.ext.coverage',
 ]
 
+# Autodoc configuration
+autodoc_member_order = 'groupwise'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
