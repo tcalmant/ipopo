95dc22bcb3d4813d140167ba253186128a55965f
Thomas Calmant <thomas.calmant@gmail.com>
2017-09-04 16:03:16 +0200

    Travis-CI: last try with prosody

---
+++ tests/prosody.cfg.lua
@@ -0,0 +1,45 @@
+admins = { }
+modules_enabled = {
+		"roster"; -- Allow users to have a roster. Recommended ;)
+		"saslauth"; -- Authentication for clients and servers. Recommended if you want to log in.
+		"tls"; -- Add support for secure TLS on c2s/s2s connections
+		"dialback"; -- s2s dialback support
+		"disco"; -- Service discovery
+		"private"; -- Private XML storage (for room bookmarks, etc.)
+		"vcard"; -- Allow users to set vCards
+		"version"; -- Replies to server version requests
+		"uptime"; -- Report how long server has been running
+		"time"; -- Let others know the time here on this server
+		"ping"; -- Replies to XMPP pings with pongs
+		"pep"; -- Enables users to publish their mood, activity, playing music and more
+		"register"; -- Allow users to register on this server using a client and change passwords
+		"admin_adhoc"; -- Allows administration via an XMPP client that supports ad-hoc commands
+		"posix"; -- POSIX functionality, sends server to background, enables syslog, etc.
+};
+
+allow_registration = false;
+daemonize = true;
+
+pidfile = "/var/run/prosody/prosody.pid";
+
+c2s_require_encryption = false
+s2s_secure_auth = false
+
+authentication = "internal_plain"
+log = {
+	-- Log files (change 'info' to 'debug' for debug logs):
+	info = "/var/log/prosody/prosody.log";
+	error = "/var/log/prosody/prosody.err";
+	-- Syslog:
+	{ levels = { "error" }; to = "syslog";  };
+}
+
+VirtualHost "localhost"
+	enabled = true
+
+------ Additional config files ------
+-- For organizational purposes you may prefer to add VirtualHost and
+-- Component definitions in their own config files. This line includes
+-- all config files in /etc/prosody/conf.d/
+
+Include "conf.d/*.cfg.lua"
