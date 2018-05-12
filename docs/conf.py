e739c663a1c9fd14317cd8678c7b3dddbd9b2da1
Thomas Calmant <thomas.calmant@gmail.com>
2018-05-12 14:12:45 +0200

    Updated copyright year and use the ReadTheDocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'iPOPO'
-copyright = '2017, Thomas Calmant'
+copyright = '2018, Thomas Calmant'
 author = 'Thomas Calmant'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -127,7 +127,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
