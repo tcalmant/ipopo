a5f1a8547d526b29ad3ce8548b01020575bf22df
Thomas Calmant <thomas.calmant@gmail.com>
2018-07-09 10:58:56 +0200

    Small reformat of some test files

--- tests/rsa/test_etcd_discovery.py
+++ tests/rsa/test_etcd_discovery.py
@@ -7,6 +7,7 @@ Tests the RSA discovery provider
 """
 # Standard library
 from pelix.rsa.endpointdescription import EndpointDescription
+
 try:
     import unittest2 as unittest
 except ImportError:
@@ -20,6 +21,7 @@ except ImportError:
 try:
     # Try to import modules
     from multiprocessing import Process, Queue
+
     # IronPython fails when creating a queue
     Queue()
 except ImportError:
@@ -37,18 +39,23 @@ from tests.utilities import WrappedProcess
 import json
 import threading
 import pelix
-from pelix.rsa import SERVICE_REMOTE_SERVICE_ADMIN,\
-    ECF_ENDPOINT_CONTAINERID_NAMESPACE
+from pelix.rsa import (
+    SERVICE_REMOTE_SERVICE_ADMIN,
+    ECF_ENDPOINT_CONTAINERID_NAMESPACE,
+)
 from pelix.framework import create_framework
 from pelix.ipopo.constants import use_ipopo
+
 # RSA
 import pelix.rsa as rsa
 from pelix.rsa.topologymanagers import TopologyManager
-from pelix.rsa.providers.discovery import SERVICE_ENDPOINT_ADVERTISER,\
-    EndpointEvent
+from pelix.rsa.providers.discovery import (
+    SERVICE_ENDPOINT_ADVERTISER,
+    EndpointEvent,
+)
 
-TEST_ETCD_HOSTNAME = 'disco.ecf-project.org'
-TEST_ETCD_TOPPATH = '/etcddiscovery.tests'
+TEST_ETCD_HOSTNAME = "disco.ecf-project.org"
+TEST_ETCD_TOPPATH = "/etcddiscovery.tests"
 
 ENDPOINT_LISTENER_SCOPE = "({0}=*)".format(ECF_ENDPOINT_CONTAINERID_NAMESPACE)
 # ------------------------------------------------------------------------------
@@ -69,40 +76,47 @@ def start_framework_for_advertise(state_queue):
     try:
         # Start the framework
         framework = create_framework(
-            ['pelix.ipopo.core',
-             'pelix.rsa.remoteserviceadmin',  # RSA implementation
-             'pelix.http.basic',  # httpservice
-             # xmlrpc distribution provider (opt)
-             'pelix.rsa.providers.distribution.xmlrpc',
-             # etcd discovery provider (opt)
-             'pelix.rsa.providers.discovery.etcd',
-             'pelix.rsa.topologymanagers.basic',
-             'samples.rsa.helloimpl_xmlrpc'],
-            {'ecf.xmlrpc.server.hostname': 'localhost',
-             'etcd.hostname': TEST_ETCD_HOSTNAME,
-             'etcd.toppath': TEST_ETCD_TOPPATH})
+            [
+                "pelix.ipopo.core",
+                "pelix.rsa.remoteserviceadmin",  # RSA implementation
+                "pelix.http.basic",  # httpservice
+                # xmlrpc distribution provider (opt)
+                "pelix.rsa.providers.distribution.xmlrpc",
+                # etcd discovery provider (opt)
+                "pelix.rsa.providers.discovery.etcd",
+                "pelix.rsa.topologymanagers.basic",
+                "samples.rsa.helloimpl_xmlrpc",
+            ],
+            {
+                "ecf.xmlrpc.server.hostname": "localhost",
+                "etcd.hostname": TEST_ETCD_HOSTNAME,
+                "etcd.toppath": TEST_ETCD_TOPPATH,
+            },
+        )
         framework.start()
 
         context = framework.get_bundle_context()
         # Start an HTTP server, required by XML-RPC
         with use_ipopo(context) as ipopo:
             ipopo.instantiate(
-                'pelix.http.service.basic.factory',
-                'http-server',
-                {'pelix.http.address': 'localhost',
-                 'pelix.http.port': 0})
+                "pelix.http.service.basic.factory",
+                "http-server",
+                {"pelix.http.address": "localhost", "pelix.http.port": 0},
+            )
 
         bc = framework.get_bundle_context()
-        rsa = bc.get_service(bc.get_service_reference(
-            'pelix.rsa.remoteserviceadmin', None))
+        rsa = bc.get_service(
+            bc.get_service_reference("pelix.rsa.remoteserviceadmin", None)
+        )
         # export the hello remote service via rsa
         # with the BasicTopologyManager, this will result
         # in publish via etcd
-        rsa.export_service(bc.get_service_reference(
-            'org.eclipse.ecf.examples.hello.IHello'),
-            {'service.exported.interfaces': '*',
-             'service.exported.configs': 'ecf.xmlrpc.server'
-             }
+        rsa.export_service(
+            bc.get_service_reference("org.eclipse.ecf.examples.hello.IHello"),
+            {
+                "service.exported.interfaces": "*",
+                "service.exported.configs": "ecf.xmlrpc.server",
+            },
         )
         # Send that we are now ready
         state_queue.put("ready")
@@ -122,7 +136,6 @@ def start_framework_for_advertise(state_queue):
 
 
 class EtcdDiscoveryListenerTest(unittest.TestCase):
-
     def setUp(self):
         """
         Starts a framework in separate process to advertise a helloimpl
@@ -131,25 +144,30 @@ class EtcdDiscoveryListenerTest(unittest.TestCase):
         """
         print(
             "EtcdDiscoveryListenerTest etcd_hostname={0},toppath={1}".format(
-                TEST_ETCD_HOSTNAME,
-                TEST_ETCD_TOPPATH))
+                TEST_ETCD_HOSTNAME, TEST_ETCD_TOPPATH
+            )
+        )
         # start external framework that publishes remote service
         self.status_queue = Queue()
-        self.publisher_process = WrappedProcess(target=start_framework_for_advertise,
-                                                args=[self.status_queue])
+        self.publisher_process = WrappedProcess(
+            target=start_framework_for_advertise, args=[self.status_queue]
+        )
         self.publisher_process.start()
         state = self.status_queue.get(10)
         self.assertEqual(state, "ready")
 
         # start a local framework
         self.framework = create_framework(
-            ['pelix.ipopo.core',
-             'pelix.rsa.remoteserviceadmin',  # RSA implementation
-             'tests.rsa.endpoint_event_listener',
-             'pelix.rsa.providers.discovery.etcd'],
-            {'etcd.hostname': TEST_ETCD_HOSTNAME,
-             'etcd.toppath': TEST_ETCD_TOPPATH
-             }
+            [
+                "pelix.ipopo.core",
+                "pelix.rsa.remoteserviceadmin",  # RSA implementation
+                "tests.rsa.endpoint_event_listener",
+                "pelix.rsa.providers.discovery.etcd",
+            ],
+            {
+                "etcd.hostname": TEST_ETCD_HOSTNAME,
+                "etcd.toppath": TEST_ETCD_TOPPATH,
+            },
         )
         self.framework.start()
         # Start the framework and return TestEndpointEventListener
@@ -158,11 +176,12 @@ class EtcdDiscoveryListenerTest(unittest.TestCase):
         with use_ipopo(context) as ipopo:
             #  create endpoint event listener
             self.listener = ipopo.instantiate(
-                'etcd-test-endpoint-event-listener-factory',
-                'etcd-test-endpoint-event-listener',
+                "etcd-test-endpoint-event-listener-factory",
+                "etcd-test-endpoint-event-listener",
                 {
                     TopologyManager.ENDPOINT_LISTENER_SCOPE: ENDPOINT_LISTENER_SCOPE
-                })
+                },
+            )
 
     def tearDown(self):
         """
@@ -183,29 +202,34 @@ class EtcdDiscoveryListenerTest(unittest.TestCase):
 
         def test_handler_1(endpoint_event, matched_filter):
             self.assertTrue(matched_filter, ENDPOINT_LISTENER_SCOPE)
-            self.assertIsNotNone(endpoint_event, 'endpoint_event is None')
+            self.assertIsNotNone(endpoint_event, "endpoint_event is None")
             self.assertTrue(isinstance(endpoint_event, EndpointEvent))
             ee_type = endpoint_event.get_type()
             self.assertTrue(
-                ee_type == EndpointEvent.ADDED or ee_type == EndpointEvent.REMOVED)
+                ee_type == EndpointEvent.ADDED
+                or ee_type == EndpointEvent.REMOVED
+            )
             ee_ed = endpoint_event.get_endpoint_description()
             self.assertTrue(isinstance(ee_ed, EndpointDescription))
             self.assertIsNotNone(
-                ee_ed.get_id(),
-                'endpoint_description id is None')
+                ee_ed.get_id(), "endpoint_description id is None"
+            )
             self.assertIsNotNone(
                 ee_ed.get_framework_uuid(),
-                'endpoint_description framework uuid is None')
+                "endpoint_description framework uuid is None",
+            )
 
             interfaces = ee_ed.get_interfaces()
             # test that service interfaces is not None and is of type list
             self.assertIsNotNone(interfaces)
             self.assertTrue(isinstance(interfaces, type([])))
             self.assertTrue(
-                'org.eclipse.ecf.examples.hello.IHello' in interfaces)
+                "org.eclipse.ecf.examples.hello.IHello" in interfaces
+            )
 
             # set the test_done_event, so tester thread will continue
             test_done_event.set()
+
         # set the handler to the test code above
         self.listener.set_handler(test_handler_1)
         # wait as much as 50 seconds to complete
@@ -222,27 +246,30 @@ class EtcdDiscoveryListenerTest(unittest.TestCase):
             elif endpoint_event.get_type() == EndpointEvent.REMOVED:
                 # do tests
                 self.assertTrue(matched_filter, ENDPOINT_LISTENER_SCOPE)
-                self.assertIsNotNone(endpoint_event, 'endpoint_event is None')
+                self.assertIsNotNone(endpoint_event, "endpoint_event is None")
                 self.assertTrue(isinstance(endpoint_event, EndpointEvent))
                 ee_ed = endpoint_event.get_endpoint_description()
                 self.assertTrue(isinstance(ee_ed, EndpointDescription))
                 self.assertIsNotNone(
-                    ee_ed.get_id(),
-                    'endpoint_description id is None')
+                    ee_ed.get_id(), "endpoint_description id is None"
+                )
                 self.assertIsNotNone(
                     ee_ed.get_framework_uuid(),
-                    'endpoint_description framework uuid is None')
+                    "endpoint_description framework uuid is None",
+                )
 
                 interfaces = ee_ed.get_interfaces()
                 # test that service interfaces is not None and is of type list
                 self.assertIsNotNone(interfaces)
                 self.assertTrue(isinstance(interfaces, type([])))
                 self.assertTrue(
-                    'org.eclipse.ecf.examples.hello.IHello' in interfaces)
+                    "org.eclipse.ecf.examples.hello.IHello" in interfaces
+                )
 
                 # finally set the test_done_event, so tester thread will
                 # continue
                 test_done_event.set()
+
         # set the handler to the test code above
         self.listener.set_handler(test_handler_2)
         # wait as much as 60 seconds to complete
@@ -250,32 +277,36 @@ class EtcdDiscoveryListenerTest(unittest.TestCase):
 
 
 class EtcdDiscoveryPublishTest(unittest.TestCase):
-
     def setUp(self):
         """
         Prepares a framework
         """
         # Create the framework
         self.framework = pelix.framework.create_framework(
-            ["pelix.ipopo.core",
-             "pelix.shell.core",
-             "pelix.http.basic",
-             "pelix.rsa.remoteserviceadmin",
-             "pelix.rsa.providers.distribution.xmlrpc",
-             'pelix.rsa.providers.discovery.etcd'],
-            {'ecf.xmlrpc.server.hostname': 'localhost',
-             'etcd.hostname': TEST_ETCD_HOSTNAME,
-             'etcd.toppath': TEST_ETCD_TOPPATH})
+            [
+                "pelix.ipopo.core",
+                "pelix.shell.core",
+                "pelix.http.basic",
+                "pelix.rsa.remoteserviceadmin",
+                "pelix.rsa.providers.distribution.xmlrpc",
+                "pelix.rsa.providers.discovery.etcd",
+            ],
+            {
+                "ecf.xmlrpc.server.hostname": "localhost",
+                "etcd.hostname": TEST_ETCD_HOSTNAME,
+                "etcd.toppath": TEST_ETCD_TOPPATH,
+            },
+        )
         self.framework.start()
 
         context = self.framework.get_bundle_context()
         # Start an HTTP server, required by XML-RPC
         with use_ipopo(context) as ipopo:
             ipopo.instantiate(
-                'pelix.http.service.basic.factory',
-                'http-server',
-                {'pelix.http.address': 'localhost',
-                 'pelix.http.port': 0})
+                "pelix.http.service.basic.factory",
+                "http-server",
+                {"pelix.http.address": "localhost", "pelix.http.port": 0},
+            )
 
         self.advertiser = None
         self.rsa = None
@@ -311,11 +342,13 @@ class EtcdDiscoveryPublishTest(unittest.TestCase):
 
     def _get_discovery_advertiser_sr(self):
         return self.framework.get_bundle_context().get_service_reference(
-            SERVICE_ENDPOINT_ADVERTISER)
+            SERVICE_ENDPOINT_ADVERTISER
+        )
 
     def _get_rsa_sr(self):
         return self.framework.get_bundle_context().get_service_reference(
-            SERVICE_REMOTE_SERVICE_ADMIN)
+            SERVICE_REMOTE_SERVICE_ADMIN
+        )
 
     def _get_service(self, sr):
         return self.framework.get_bundle_context().get_service(sr)
@@ -324,8 +357,7 @@ class EtcdDiscoveryPublishTest(unittest.TestCase):
         self.framework.get_bundle_context().unget_service(sr)
 
     def _get_advertiser(self):
-        self.advertiser = self._get_service(
-            self._get_discovery_advertiser_sr())
+        self.advertiser = self._get_service(self._get_discovery_advertiser_sr())
         return self.advertiser
 
     def _get_rsa(self):
@@ -341,31 +373,37 @@ class EtcdDiscoveryPublishTest(unittest.TestCase):
 
     def _export_svc(self):
         self.svc_reg = self._register_svc()
-        self.export_reg = self._get_rsa().export_service(self.svc_reg.get_reference(), {
-            rsa.SERVICE_EXPORTED_INTERFACES: '*',
-            rsa.SERVICE_EXPORTED_CONFIGS: "ecf.xmlrpc.server"
-        })[0]
+        self.export_reg = self._get_rsa().export_service(
+            self.svc_reg.get_reference(),
+            {
+                rsa.SERVICE_EXPORTED_INTERFACES: "*",
+                rsa.SERVICE_EXPORTED_CONFIGS: "ecf.xmlrpc.server",
+            },
+        )[0]
         return self.export_reg
 
     def test_get_discovery_advertiser(self):
         disc_adv_sr = self._get_discovery_advertiser_sr()
-        self.assertIsNotNone(disc_adv_sr, 'advertiser ref is null')
+        self.assertIsNotNone(disc_adv_sr, "advertiser ref is null")
         self.advertiser = self._get_service(disc_adv_sr)
-        self.assertIsNotNone(self.advertiser, 'advertiser svc is null')
+        self.assertIsNotNone(self.advertiser, "advertiser svc is null")
 
     def test_none_advertised(self):
         adv = self._get_advertiser()
         eps = adv.get_advertised_endpoints()
         self.assertDictEqual(
-            eps, {}, 'advertised endpoints not empty eps={0}'.format(eps))
+            eps, {}, "advertised endpoints not empty eps={0}".format(eps)
+        )
 
     def test_etcd_session(self):
-        self.assertIsNotNone(self._get_advertiser(
-        )._sessionid, 'etcd._sessionid is null')
+        self.assertIsNotNone(
+            self._get_advertiser()._sessionid, "etcd._sessionid is null"
+        )
 
     def test_etcd_client(self):
-        self.assertIsNotNone(self._get_advertiser()._client,
-                             'etcd._client is null')
+        self.assertIsNotNone(
+            self._get_advertiser()._client, "etcd._client is null"
+        )
 
     def test_etcd_remote_exists(self):
         adv = self._get_advertiser()
@@ -377,25 +415,30 @@ class EtcdDiscoveryPublishTest(unittest.TestCase):
         ed = export_reg.get_description()
         ed_id = ed.get_id()
         ep_adv = adv.advertise_endpoint(ed)
-        self.assertTrue(ep_adv, 'advertise_endpoint failed')
-        ep_key = adv._get_session_path() + '/' + ed_id
+        self.assertTrue(ep_adv, "advertise_endpoint failed")
+        ep_key = adv._get_session_path() + "/" + ed_id
         # test for existence of ep id key
         adv._client.get(ep_key)
         # get advertised endpoints
         eps = adv.get_advertised_endpoints()
         # should be of length 1
-        self.assertTrue(len(eps) == 1, 'length of eps is not equal 1')
+        self.assertTrue(len(eps) == 1, "length of eps is not equal 1")
         # now unadvertise
         adv.unadvertise_endpoint(ed_id)
         try:
             adv._client.get(ep_key)
             self.fail(
-                'endpoint={0} still advertised after being removed'.format(ed_id))
-        except Exception:            # exception expected
+                "endpoint={0} still advertised after being removed".format(
+                    ed_id
+                )
+            )
+        except Exception:  # exception expected
             pass
         eps = adv.get_advertised_endpoints()
         self.assertTrue(
-            len(eps) == 0, 'length of eps should be 0 and is {0}'.format(len(eps)))
+            len(eps) == 0,
+            "length of eps should be 0 and is {0}".format(len(eps)),
+        )
 
     def test_etcd_advertise_content(self):
         adv = self._get_advertiser()
@@ -407,15 +450,20 @@ class EtcdDiscoveryPublishTest(unittest.TestCase):
         # advertise it
         adv.advertise_endpoint(ed)
         # get the string directly via http and key
-        ed_val_str = list(adv._client.get(
-            adv._get_session_path() + '/' + ed_id).get_subtree())[0].value
+        ed_val_str = list(
+            adv._client.get(adv._get_session_path() + "/" + ed_id).get_subtree()
+        )[0].value
         # decode the string into json object (dict)
         val_encoded = json.loads(ed_val_str)
         # compare the original dict with the one returned
-        self.assertDictEqual(encoded_ep, val_encoded,
-                             'encoded endpoints not equal')
+        self.assertDictEqual(
+            encoded_ep, val_encoded, "encoded endpoints not equal"
+        )
         # also check a couple of fields
-        self.assertListEqual(encoded_ep['properties'], val_encoded['properties'],
-                             'encoded_ed_props and val_encoded_props are not equal')
+        self.assertListEqual(
+            encoded_ep["properties"],
+            val_encoded["properties"],
+            "encoded_ed_props and val_encoded_props are not equal",
+        )
         # now unadvertise
         adv.unadvertise_endpoint(ed_id)
