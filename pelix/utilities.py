d8df797c862086533f84f973f87b2934fe75d6e3
Angelo Cutaia <48996402+Angeloxx92@users.noreply.github.com>
2020-01-28 22:15:17 +0100

    restore comprehension list

--- pelix/utilities.py
+++ pelix/utilities.py
@@ -348,7 +348,10 @@ def remove_duplicates(items):
         return items
 
     new_list = []
-    return [new_list.append(item) for item in items if item not in new_list]
+    for item in items:
+        if item not in new_list:
+            new_list.append(item)
+    return new_list
 
 
 # ------------------------------------------------------------------------------
