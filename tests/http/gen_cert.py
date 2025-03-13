90c3e51d9c81306c279752b856e17747dd813e99
Thomas Calmant <thomas.calmant@gmail.com>
2025-03-13 18:29:33 +0100

    Mark the CA basic constraint as critical

--- tests/http/gen_cert.py
+++ tests/http/gen_cert.py
@@ -95,8 +95,7 @@ x509_extensions = v3_ca
 [ v3_ca ]
 subjectKeyIdentifier=hash
 authorityKeyIdentifier=keyid:always,issuer:always
-#basicConstraints = critical,CA:true
-basicConstraints = CA:true
+basicConstraints = critical,CA:true
 """
         )
 
