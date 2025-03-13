2eb57b4aa06b18495f298f98365c3c1a4a718f04
Thomas Calmant <thomas.calmant@gmail.com>
2025-03-13 19:08:29 +0100

    Added CA certification key usage

--- tests/http/gen_cert.py
+++ tests/http/gen_cert.py
@@ -95,7 +95,8 @@ x509_extensions = v3_ca
 [ v3_ca ]
 subjectKeyIdentifier=hash
 authorityKeyIdentifier=keyid:always,issuer:always
-basicConstraints = critical,CA:true
+basicConstraints = critical, CA:true
+keyUsage = critical, keyCertSign, cRLSign
 """
         )
 
