82513cd8b2be787b3e118235124bce09a358f658
Thomas Calmant <thomas.calmant@gmail.com>
2026-04-28 21:10:48 +0200

    Mark support for Python 3.14
    Signed-off-by: Thomas Calmant <thomas.calmant@gmail.com>

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ],
     install_requires=["jsonrpclib-pelix>=0.4.3"],
